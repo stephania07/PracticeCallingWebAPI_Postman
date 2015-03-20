ed7dd40af482a8668a09983fa481d309eb1b68cd
stephania07 <stephaniaiyob@yahoo.com>
2015-03-19 22:59:04 -0500

    webApi_postman functioning

---
+++ ProductStore/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        ///GET: /api/products
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+        }
+
+        // GET: /api/products/id
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        //GET: /api/products?category=category
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        //Post: /api/products/item
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        //Put: /api/products/id/product
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        //Delete: /api/products/id
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(id);
+        }
+    }
+}
